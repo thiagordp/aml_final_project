7ee9383fd7cb74f170937d3a151e26d951943354
Thiago Raulino Dal Pont <thiagordalpont@gmail.com>
2021-12-03 18:19:13 -0300

    Progress on EDA

--- eda/after_proc_eda.py
+++ eda/after_proc_eda.py
@@ -27,7 +27,7 @@ def most_frequent_crimes():
 
     for crime_row, label in zip(list_crimes, list_labels):
         if label not in dict_crimes.keys():
-            dict_crimes[label]= {}
+            dict_crimes[label] = {}
 
         for crime in crime_row:
             if crime in dict_crimes[label].keys():
@@ -43,9 +43,9 @@ def most_frequent_crimes():
     out_path = os.path.join(PATH_OUTPUT_EDA, "crimes_count.xlsx")
     df.to_excel(out_path, index=False)
 
-    fig, ax = plt.subplots(figsize=(17, 8))
+    fig, ax = plt.subplots(figsize=(15, 7))
 
-    ax.grid(ls="..")
+    ax.grid(ls="--")
     sns.barplot(
         data=df,
         y="Crime", x="Contagem", hue="Resultado"
@@ -60,7 +60,8 @@ def most_frequent_crimes():
 def most_frequent_crimes_by_year():
     # Maybe place the label here as well
     df = pd.read_csv(PATH_PLANILHA_PROC.replace("@ext", "csv"))
-
+    print("-" * 50)
+    print("Most frequent crimes by year")
     print(df.describe())
     print(df.info())
 
@@ -106,7 +107,6 @@ def most_frequent_crimes_by_year():
                     data=df,
                     ax=ax)
 
-
     plt.tight_layout()
     plt.legend([], [], frameon=False)
 
@@ -114,6 +114,70 @@ def most_frequent_crimes_by_year():
     plt.savefig(out_path, dpi=200)
 
 
+def most_frequent_rappourter():
+    df = pd.read_csv(PATH_PLANILHA_PROC.replace("@ext", "csv"))
+    print("-" * 50)
+    print("Most frequent rappourter")
+    print(df.describe())
+    print(df.info())
+
+    list_rappourter = list(df["Relator"])
+
+    dict_rappourter = {}
+    for rapp in sorted(list_rappourter):
+        if rapp not in sorted(dict_rappourter.keys()):
+            dict_rappourter[rapp] = 1
+        else:
+            dict_rappourter[rapp] += 1
+
+    data = []
+    for key in dict_rappourter.keys():
+        data.append([key, dict_rappourter[key]])
+    df = pd.DataFrame(data, columns=["Relator", "Contagem"])
+    fig, axes = plt.subplots(1, 1, figsize=(14, 9))
+    plt.title("Rapporteur Histogram")
+    sns.barplot(data=df, x="Contagem", y="Relator", ax=axes, palette=sns.color_palette(['#1f77b4']))
+    plt.tight_layout()
+    out_path = os.path.join(PATH_OUTPUT_EDA, "histogram_rappourter.png")
+    plt.savefig(out_path, dpi=200)
+    plt.show()
+
+
+def most_frequent_rappourter_by_label():
+    df = pd.read_csv(PATH_PLANILHA_PROC.replace("@ext", "csv"))
+    print("-" * 50)
+    print("Most frequent rappourter")
+    print(df.describe())
+    print(df.info())
+
+    dict_rappourter = {}
+
+    for rapp, label in zip(df["Relator"], df["Resultado Doc"]):
+        if label not in dict_rappourter.keys():
+            dict_rappourter[label] = {}
+
+        if rapp not in dict_rappourter[label].keys():
+            dict_rappourter[label][rapp] = 1
+        else:
+            dict_rappourter[label][rapp] += 1
+
+    data = []
+    for label in sorted(dict_rappourter.keys()):
+        for rapp in sorted(dict_rappourter[label].keys()):
+            data.append([rapp, label, dict_rappourter[label][rapp]])
+    df = pd.DataFrame(data, columns=["Relator","Resultado", "Contagem"])
+    fig, axes = plt.subplots(1, 1, figsize=(14, 9))
+
+    plt.title("Rapporteur Histogram by Label")
+    sns.barplot(data=df, x="Contagem", y="Relator",hue="Resultado", ax=axes)
+    plt.tight_layout()
+    out_path = os.path.join(PATH_OUTPUT_EDA, "histogram_rappourter_label.png")
+    plt.savefig(out_path, dpi=200)
+    plt.show()
+
+
 def eda_after_proc_part_i():
     most_frequent_crimes()
     most_frequent_crimes_by_year()
+    most_frequent_rappourter()
+    most_frequent_rappourter_by_label()
